a3323d95121c15e538c468254aa0dcbfdea0ab09
ComeSarazin <sasacoco@orange.fr>
2023-12-12 15:37:22 +0100

    coucou sami

---
+++ service/maMeteoService.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Service;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
+
+
+class maMeteoService
+{
+        public function __construct(
+            private HttpClientInterface $client,
+        ) {
+        }
+
+    public function addMeteo (){
+        $response = $this->client->request(
+            'GET',
+            'https://api.openweathermap.org/data/2.5/weather?q={bordeaux}&appid={21286764396e4f0a39815346959382b5}'
+        );
+
+        $statusCode = $response->getStatusCode();
+        // $statusCode = 200
+        $contentType = $response->getHeaders()['content-type'][0];
+        // $contentType = 'application/json'
+        $content = $response->getContent();
+        // $content = '{"id":521583, "name":"symfony-docs", ...}'
+        $content = $response->toArray();
+        // $content = ['id' => 521583, 'name' => 'symfony-docs', ...]
+        var_dump($content);
+        return $content;
+       
+    }
+    
+}
